1e3140c2ea8c66370034fbc14479914e3ffb17ee
Connor Lagana <connor.lagana@gmail.com>
2020-04-29 00:15:51 -0400

    Initial Commit

---
+++ AnimatedBarChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedBarChart
+//
+//  Created by Connor Lagana on 4/29/20.
+//  Copyright © 2020 Connor Lagana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
